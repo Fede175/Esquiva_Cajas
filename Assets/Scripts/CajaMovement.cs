6e4f4271aaefb3e654515540f7f3f5963d46b5ea
FEDERICO CASTELLANO <Militoplayer@gmail.com>
2026-05-07 10:18:13 -0300

    Caja Movement Implemented

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float Velocidad = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-Velocidad * Time.deltaTime ,0);
+    }
+}
